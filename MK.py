1aa9581002dff30f6564d0b509ada2d9406c0066
Martin Waitz <tali@admingilde.org>
2010-04-22 14:06:32 +0200

    rewrite command handling

--- MK.py
+++ MK.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 
+import asyncore
 import os
+import select
+import struct
 import termios
+import time
 
 _zero = ord('=')
 
@@ -15,6 +20,96 @@ def board_name(addr):
         return "MK3Mag"
     return "<unknown>"
 
+
+settings = [
+    "Channel_Gas", "Channel_Gier", "Channel_Nick", "Channel_Roll",
+    "POTI1", "POTI2", "POTI3", "POTI4", "POTI5", "POTI6", "POTI7", "POTI8",
+    "GlobalConfig",            # 0x01=Höhenregler aktiv,0x02=Kompass aktiv, 0x04=GPS aktiv, 0x08=Heading Hold aktiv
+    "Hoehe_MinGas",            # Wert : 0-100
+    "Luftdruck_D",             # Wert : 0-250
+    "MaxHoehe",                # Wert : 0-32
+    "Hoehe_P",                 # Wert : 0-32
+    "Hoehe_Verstaerkung",      # Wert : 0-50
+    "Hoehe_ACC_Wirkung",       # Wert : 0-250
+    "Hoehe_HoverBand",         # Wert : 0-250
+    "Hoehe_GPS_Z",             # Wert : 0-250
+    "Hoehe_StickNeutralPoint", # Wert : 0-250
+    "Stick_P",                 # Wert : 1-6
+    "Stick_D",                 # Wert : 0-64
+    "Gier_P",                  # Wert : 1-20
+    "Gas_Min",                 # Wert : 0-32
+    "Gas_Max",                 # Wert : 33-250
+    "GyroAccFaktor",           # Wert : 1-64
+    "KompassWirkung",          # Wert : 0-32
+    "Gyro_P",                  # Wert : 10-250
+    "Gyro_I",                  # Wert : 0-250
+    "Gyro_D",                  # Wert : 0-250
+    "Gyro_Gier_P",             # Wert : 10-250
+    "Gyro_Gier_I",             # Wert : 0-250
+    "UnterspannungsWarnung",   # Wert : 0-250
+    "NotGas",                  # Wert : 0-250     //Gaswert bei Empängsverlust
+    "NotGasZeit",              # Wert : 0-250     // Zeitbis auf NotGas geschaltet wird, wg. Rx-Problemen
+    "Receiver",                # 0= Summensignal, 1= Spektrum, 2 =Jeti, 3=ACT DSL, 4=ACT S3D
+    "I_Faktor",                # Wert : 0-250
+    "UserParam1",              # Wert : 0-250
+    "UserParam2",              # Wert : 0-250
+    "UserParam3",              # Wert : 0-250
+    "UserParam4",              # Wert : 0-250
+    "ServoNickControl",        # Wert : 0-250     // Stellung des Servos
+    "ServoNickComp",           # Wert : 0-250     // Einfluss Gyro/Servo
+    "ServoNickMin",            # Wert : 0-250     // Anschlag
+    "ServoNickMax",            # Wert : 0-250     // Anschlag
+    "ServoRollControl",        # Wert : 0-250     // Stellung des Servos
+    "ServoRollComp",           # Wert : 0-250
+    "ServoRollMin",            # Wert : 0-250
+    "ServoRollMax",            # Wert : 0-250
+    "ServoNickRefresh",        # Speed of the Servo
+    "Servo3",                  # Value or mapping of the Servo Output
+    "Servo4",                  # Value or mapping of the Servo Output
+    "Servo5",                  # Value or mapping of the Servo Output
+    "LoopGasLimit",            # Wert: 0-250  max. Gas während Looping
+    "LoopThreshold",           # Wert: 0-250  Schwelle für Stickausschlag
+    "LoopHysterese",           # Wert: 0-250  Hysterese für Stickausschlag
+    "AchsKopplung1",           # Wert: 0-250  Faktor, mit dem Gier die Achsen Roll und Nick koppelt (NickRollMitkopplung)
+    "AchsKopplung2",           # Wert: 0-250  Faktor, mit dem Nick und Roll verkoppelt werden
+    "CouplingYawCorrection",   # Wert: 0-250  Faktor, mit dem Nick und Roll verkoppelt werden
+    "WinkelUmschlagNick",      # Wert: 0-250  180°-Punkt
+    "WinkelUmschlagRoll",      # Wert: 0-250  180°-Punkt
+    "GyroAccAbgleich",         # 1/k  (Koppel_ACC_Wirkung)
+    "Driftkomp",
+    "DynamicStability",
+    "UserParam5",              # Wert : 0-250
+    "UserParam6",              # Wert : 0-250
+    "UserParam7",              # Wert : 0-250
+    "UserParam8",              # Wert : 0-250
+    "J16Bitmask",              # for the J16 Output
+    "J16Timing",               # for the J16 Output
+    "J17Bitmask",              # for the J17 Output
+    "J17Timing",               # for the J17 Output
+    "WARN_J16_Bitmask",        # for the J16 Output
+    "WARN_J17_Bitmask",        # for the J17 Output
+    "NaviGpsModeControl",      # Parameters for the Naviboard
+    "NaviGpsGain",
+    "NaviGpsP",
+    "NaviGpsI",
+    "NaviGpsD",
+    "NaviGpsPLimit",
+    "NaviGpsILimit",
+    "NaviGpsDLimit",
+    "NaviGpsACC",
+    "NaviGpsMinSat",
+    "NaviStickThreshold",
+    "NaviWindCorrection",
+    "NaviSpeedCompensation",
+    "NaviOperatingRadius",
+    "NaviAngleLimitation",
+    "NaviPH_LoginTime",
+    "ExternalControl",        # for serial Control
+    "BitConfig",          # (war Loop-Cfg) Bitcodiert: 0x01=oben, 0x02=unten, 0x04=links, 0x08=rechts / wird getrennt behandelt
+    "ServoCompInvert",    # 0x01 = Nick, 0x02 = Roll   0 oder 1  // WICHTIG!!! am Ende lassen
+    "ExtraConfig",        # bitcodiert
+#   char Name[12];
+    ]
 
 def calc_crc(data):
     counter = ord('#')
@@ -85,109 +180,224 @@ def encode_str(s, l):
         l -= 1
     return data
 
+
+
+class Board(object):
+    def __init__(self):
+        self.redirect = None
+
+
+class FlightCtrl(Board):
+    def __init__(self):
+        Board.__init__(self)
+        self.addr = 'b'
+        self.name = "FlightCtrl"
+        self.redirect = 0
+
+class NaviCtrl(Board):
+    def __init__(self):
+        Board.__init__(self)
+        self.addr = 'c'
+        self.name = "NaviCtrl"
+
+class MK3Mag(Board):
+    def __init__(self):
+        Board.__init__(self)
+        self.addr = 'd'
+        self.name = "MK3Mag"
+        self.redirect = 1
+
+class MKGPS(Board):
+    def __init__(self):
+        Board.__init__(self)
+        self.name = "MKGPS"
+        self.redirect = 2
+
+
+
+class Command(object):
+    def __init__(self, board, id, reply_id, data):
+        self.board = board
+        self.id = id
+        self.tries = 3
+        packet = board.addr + id + encode(data)
+        self.frame = '#' + packet + calc_crc(packet) + '\r'
+        self.reply_id = reply_id
+        self.reply = None
+
+    def get_frame(self):
+        return self.frame
+
+    def parse_reply(self, reply):
+        return reply
+
+class CmdVersion(Command):
+    def __init__(self, board):
+        Command.__init__(self, board, 'v', 'V', [])
+
+    def parse_reply(self, reply):
+        return "%d.%d%s" % ( reply[0], reply[1], chr( ord('a')+ reply[4]) )
+
+class CmdAnalogLabel(Command):
+    def __init__(self, board, index):
+        Command.__init__(self, board, 'a', 'A', [index])
+
+    def parse_reply(self, reply):
+        return reply[0], decode_str(reply[1:])
+
+class CmdGetSettings(Command):
+    def __init__(self, board, index):
+        Command.__init__(self, board, 'q', 'Q', [0xff])
+
+    def parse_reply(self, reply):
+        return reply[0], reply[1], reply[2:]
+#        print "Settings", data[0], "Version", data[1]
+#        settings = {}
+#        for (name, value) in zip(settings, data[2:]):
+#            unsigned = value
+#            if unsigned < 0: unsigned += 256
+#
+#            print "%25s: %4d  0x%02x" % (name, value, unsigned)
+
+class CmdGetChannels(Command):
+    def __init__(self, board):
+        Command.__init__(self, board, 'p', 'P', [])
+
+    def parse_reply(self, reply):
+        return struct.unpack("hhhhhhh", map(chr,reply[:14]))
+
+
+class CmdSetMotorMixer(Command):
+    def __init__(self, board, name, data):
+        payload = [1] + encode_str(name, 12) + data
+        Command.__init__(self, board, 'm', 'M', payload)
+
+    def parse_reply(self, reply):
+        return reply[0]
+
+
+class CmdGetMotorMixer(Command):
+    def __init__(self, board):
+        Command.__init__(self, board, 'n', 'N', [])
+
+    def parse_reply(self, reply):
+        if reply[0] != 1:
+            raise "Unknown mixer revision"
+        return decode_str(reply[1:13]), reply[13:]
+
+
 class MK(object):
-    pass
+    def __init__(self, device):
+        self.selected = None
 
-def send_packet(link, addr, id, contents):
-    code = encode(contents)
-    packet = addr + id + code
-    crc = calc_crc(packet)
-    link.write('#')
-    link.write(packet)
-    link.write(crc)
-    link.write('\r')
+        self.device = open(device, "r+b")
+        fd = self.device.fileno()
 
-def recv_packet(link, packet):
-    crc = calc_crc(packet[:-2])
-    if crc != packet[-2:]:
-        print "CRC!", crc, packet
-        return
+        tca = termios.tcgetattr( fd )
+        tca[4] = termios.B57600
+        tca[5] = termios.B57600
+        termios.tcsetattr( fd, termios.TCSAFLUSH, tca )
 
-    addr = packet[0]
-    id = packet[1]
-    data = decode(packet[2:-2])
+        self.line = ""
 
+        self.fc = FlightCtrl()
+        self.nc = NaviCtrl()
+        self.gps = MKGPS()
+        self.mk3mag = MK3Mag()
 
-    if id == 'w':
-        #print "HeadingRequest", data
-        pass
-
-    elif id == 'A':
-        index = data[0]
-        label = decode_str(data[1:])
-        print "Label", index, '"%s"' % label
-        send_packet(link, 'a', 'a', [ index+1 ])
-
-    elif id == 'M':
-        if data[0] == 1:
-            print "Mixer write OK"
+    def recv_data(self, timeout):
+        fd = self.device.fileno()
+        r, w, e = select.select([fd], [], [], timeout)
+        if len(r):
+            return os.read(fd, 4096)
         else:
-            print "Mixer write ERROR"
+            return ""
 
-    elif id == 'N':
-        rev = data[0]
-        label = decode_str(data[1:13])
-        print "Mixer", rev, label
-        if rev != 1:
-            raise "unkown mixer revision"
-        table = data[13:]
-        print "Motor", "Gas", "Nick", "Roll", "Yaw"
-        motor = 1
-        while table[0]:
-            print motor, table[0], table[1], table[2], table[3]
-            table = table[4:]
-            motor += 1
+    def recv_packet(self, board, id, timeout):
 
-    elif id == 'Q':
-        print "Settings", data
+        until = time.time() + timeout
 
-    elif id == 'V':
-        print "Version", "%s %d.%d%s" % ( board_name(addr),
-                data[0], data[1], chr( ord('a')+ data[4]) )
-        print "Serial", data[2], data[3]
+        received = ""
+        while timeout > 0:
+            if not received:
+                received = self.recv_data(timeout)
+                if not received: return None
+                #print "received", len(received)
+                #print ">>>", received, "<<<"
+                timeout = until - time.time()
+                #print "timeout", timeout
 
-    else:
-        print "unknown", addr, id, data
+            c = received[0]
+            received = received[1:]
 
+            if c != '\r':
+                self.line += c
+                continue
 
-def connect(device):
-    link = open(device, "r+b")
-    fd = link.fileno()
+            if len(self.line) == 0: continue
 
-    tca = termios.tcgetattr( link.fileno() )
-    tca[4] = termios.B57600
-    tca[5] = termios.B57600
-    termios.tcsetattr( link.fileno(), termios.TCSAFLUSH, tca )
+            if self.line[0] != '#':
+                print "line in:", self.line
+                self.line = ""
+                continue
 
-    return link
+            packet = self.line[1:]
+            self.line = ""
+            #print "received_packet"
+            crc = calc_crc(packet[:-2])
+            if crc != packet[-2:]:
+                print "CRC!", crc, packet
+                continue
 
-def receive(link):
-    state = 0
-    line = ""
-    while True:
-        data = link.read( 1 )
-        if state == 0:
-            if data == '#':
-                state = 1
-            elif data == '\r':
-                print "line in:", line
-                line = ""
+            r_addr = packet[0]
+            r_id = packet[1]
+            r_data = decode(packet[2:-2])
+
+            if r_addr == board.addr and r_id == id:
+                return r_data
             else:
-                line += data
-        elif state == 1:
-            if data == '\r':
-                recv_packet(link, line)
-                line = ""
-                state = 0
-            else:
-                line += data
+                print "unexpected packet", r_addr, r_id, r_data
+        return None
 
 
+    def send_data(self, data, wait):
+        wrote = os.write(self.device.fileno(), data)
+        #print "send_data", wrote, data
 
-#    data = link.read( 64 )
-#    print "in:", data
-#    print "-->", decode(data[2:-3])
+        if wait:
+            time.sleep(wait)
 
-#for line in link:
-#    print line
+
+    def select_board(self, board):
+        if board == self.selected: return
+        #print "select_board"
+
+        # cancel existing redirections
+        escape = "#" + chr(0x1b) + chr(0x1b)
+        escape += chr(0x55) + chr(0xaa) + chr(0) + '\r'
+        self.send_data(escape, 0.1)
+
+        if board.redirect != None:
+            redirect = Command(self.nc, 'u', None, [board.redirect])
+            self.send_data(redirect.get_frame(), 0.1)
+
+        self.selected = board
+
+
+    def send_cmd(self, cmd):
+        self.select_board(cmd.board)
+        self.line = ""
+
+        reply = None
+        while not reply and cmd.tries:
+            self.send_data(cmd.get_frame(), 0)
+            reply = self.recv_packet(cmd.board, cmd.reply_id, 0.5)
+            cmd.tries -= 1
+
+        if reply:
+            return cmd.parse_reply(reply)
+        else:
+            return None
+
+
 
